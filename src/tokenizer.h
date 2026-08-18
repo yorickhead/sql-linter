5ccf4ad1a27d15c25d96cb353a6f0e2d2c9428e0
osami <v.pylayev@mail.ru>
2026-08-18 09:15:04 +0500

    add tokenizer.h

---
+++ src/tokenizer.h
@@ -0,0 +1,26 @@
+#include <stddef.h>
+
+enum TokenType {
+  LBRACE,         // "("
+  RBRACE,         // ")"
+  COMMA,          // ","
+  SEMICOLON,      // ";"
+  GREATER,        // ">"
+  LESS,           // "<"
+  EQUAL,          // "="
+  QUOTATION_MARK, // "'"
+  ASTERISK,       // "*"
+  STRING,
+  NUMBER,
+
+  UNKNOWN,
+  EOF_TOKEN
+};
+
+typedef struct Token {
+  enum TokenType type;
+  char *start;
+  size_t len;
+} Token;
+
+Token *tokenize(char *data);
